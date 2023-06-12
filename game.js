c5c3dc86da5403e89e91ced8eb6d55c469571038
Michal Kwiatek <m.kwiatek@digitalattitude.com>
2023-06-12 18:56:02 +0200

    Initialize canvas

--- game.js
+++ game.js
@@ -1,5 +1,24 @@
 (function () {
 
-  console.log('test')
+  const CANVAS_WIDTH = 500
+  const CANVAS_HEIGHT = 500
+
+  const canvas = document.getElementById('game-canvas')
+  canvas.width = CANVAS_WIDTH
+  canvas.height = CANVAS_HEIGHT
+
+  const ctx = canvas.getContext('2d')
+
+  function animate() {
+    ctx.fillStyle = 'green'
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+
+    ctx.fillStyle = 'red'
+    ctx.fillRect(0, 0, 20, 20)
+
+    requestAnimationFrame(animate)
+  }
+
+  animate()
 
 })()
